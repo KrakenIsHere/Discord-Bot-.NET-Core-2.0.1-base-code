c285460b8a688372f40fe46ab0003132b653e0a1
Kristian K. S. Nielsen <kris9463@edu.sde.dk>
2018-08-24 13:00:28 +0200

    Bot Basics Complete
    Setup for basics and launch for discord bot set and working (tested)

---
+++ DiscordBot.NETCore2.0/Commands.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Discord;
+using Discord.Commands;
+
+namespace DiscordBot.NETCore2._0
+{
+    public class Commands : ModuleBase
+    {
+        [Command("Hello")] //Command
+        [Summary("Says Ello!!")] //Description
+        [Alias("Hi")] //Other uses for this command
+        public async Task Hello([Remainder] string remainder) //Remainder come after the command itself
+        {
+            if (remainder == "1")
+            {
+                await Context.Channel.SendMessageAsync("Ello!!");
+            }
+            else if (remainder == "2")
+            {
+                await Context.Channel.SendMessageAsync("Hello :grinning:");
+            }
+            else
+            {
+                await Context.Channel.SendMessageAsync("That's not a valid number :crying_cat_face:");
+            }
+        }
+    }
+}
---
+++ DiscordBot.NETCore2.0/Program.cs
@@ -0,0 +1,77 @@
+﻿using Discord;
+using Discord.Commands;
+using Discord.WebSocket;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
+
+namespace DiscordBot.NETCore2._0
+{
+    class Program : ModuleBase
+    {
+        public static DiscordSocketClient client;
+        private CommandService commands;
+
+        string token = ""; //Your token here!
+
+        public IServiceProvider ConfigServ()
+        {
+            var services = new ServiceCollection()
+                .AddSingleton(client)
+                .AddSingleton(commands)
+                .BuildServiceProvider();
+
+            return services;
+        }
+
+        public static void Main(string[] args) => new Program().MainAsync().GetAwaiter().GetResult();
+
+        public async Task MainAsync()
+        {
+            client = new DiscordSocketClient();
+            commands = new CommandService();
+
+            await InstallCommands();
+
+            client.Log += Log;
+
+            await client.LoginAsync(TokenType.Bot, token);
+            await client.StartAsync();
+
+            await Task.Delay(-1);
+        }
+
+        public async Task InstallCommands()
+        {
+            client.MessageReceived += HandleCommand;
+            await commands.AddModulesAsync(Assembly.GetEntryAssembly());
+        }
+
+        private async Task HandleCommand(SocketMessage arg)
+        {
+            var msg = arg as SocketUserMessage;
+            if (msg == null) return;
+            var context = new CommandContext(client, msg);
+            int pos = 0;
+
+            //Set Custom Prefix here
+            if (msg.HasCharPrefix('!', ref pos)) //This char is your prefix (The symbol before a command)
+            {
+                var result = await commands.ExecuteAsync(context, pos, ConfigServ());
+
+                if (!result.IsSuccess && result.Error != CommandError.UnknownCommand)
+                {
+                    Console.WriteLine(result.ErrorReason);
+                }
+            }
+        }
+
+        private Task Log(LogMessage logmessage)
+        {
+            Console.WriteLine(logmessage.ToString());
+            return Task.CompletedTask;
+        }
+    }
+}
