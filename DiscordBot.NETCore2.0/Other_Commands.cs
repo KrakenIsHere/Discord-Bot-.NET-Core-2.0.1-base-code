d098aa83ca49d533c0a110a150a569a251fc0b79
Kristian K. S. Nielsen <kris9463@edu.sde.dk>
2018-08-27 08:55:33 +0200

    Get user bit added
    Added an example of userinfo gathering

--- DiscordBot.NETCore2.0/Other_Commands.cs
+++ DiscordBot.NETCore2.0/Other_Commands.cs
@@ -12,18 +12,32 @@ namespace DiscordBot.NETCore2._0
         [Command("Help")] //Command
         public async Task Help()
         {
-            var eb = new EmbedBuilder();
+            var eb = new EmbedBuilder(); //builds an embedded message
 
-            eb.WithTitle("Help Menu:")
-              .WithDescription("Help menu here")
-              .WithColor(Color.LightOrange)
-              .AddField("Field 1", "Field 1 Description")
+            eb.WithTitle("Help Menu:") //Title of the embedded message
+              .WithDescription("Help menu here") //Description of the embedded message
+              .WithColor(Color.LightOrange) //Color of the embedded message side line
+              .AddField("Field 1", "Field 1 Description") //A field with description for the embedded message
               .AddField("Field 2", "Field 2 Description")
               .AddField("Field 3", "Field 3 Description")
               .AddField("Field 4", "Field 4 Description");
 
 
-            await Context.Channel.SendMessageAsync("", false, embed: eb.Build());
+            await Context.Channel.SendMessageAsync("", false, embed: eb.Build()); //Sends an embedded message
+        }
+
+        [Command("Test")] //Command
+        public async Task Test([Remainder] IUser user) //IUser gets the input as a UserName
+        {
+            string[] output = new string[]
+            {
+                $"Name: {user.Username}",
+                $"Activity: {user.Activity}",
+                $"Status: {user.Status}"
+            };
+
+
+            await Context.Channel.SendMessageAsync(output.ToString()); //Sends a message
         }
     }
 }
